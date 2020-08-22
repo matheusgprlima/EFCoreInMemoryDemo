47e10cade8f032ef2d0a3c36b1e49a1640f0097b
Matheus Gomes <matheusgprlima@gmail.com>
2020-08-22 19:04:51 -0300

    Adição Ícones.

--- sql/CriacaoTabelasSistema.sql
+++ sql/CriacaoTabelasSistema.sql
@@ -11,7 +11,7 @@ GO
 
 CREATE TABLE [BoardGames] (
     [Id] uniqueidentifier NOT NULL,
-    [Title] varchar(200) NOT NULL,
+    [Title] nvarchar(200) NOT NULL,
     [PublishingCompany] varchar(200) NOT NULL,
     [MinPlayers] int NOT NULL,
     [MaxPlayers] int NOT NULL,
@@ -21,8 +21,12 @@ CREATE TABLE [BoardGames] (
 
 GO
 
+CREATE UNIQUE INDEX [IX_BoardGames_Title] ON [BoardGames] ([Title]) WHERE [Title] IS NOT NULL;
+
+GO
+
 INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20200822013552_Initial', N'3.1.7');
+VALUES (N'20200822210424_Initial', N'3.1.7');
 
 GO
 
